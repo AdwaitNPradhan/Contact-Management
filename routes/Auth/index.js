3a6e4fddf12310195e42061a4168e75fefb585e1
AdwaitNPradhan <adwaitpradhan2001@gmail.com>
2021-10-05 15:48:54 +0530

    Sign Out implemented; User State Check implemented on AUTH routes

--- routes/Auth/index.js
+++ routes/Auth/index.js
@@ -2,11 +2,35 @@ const { SignIn } = require("./SignIn");
 const { SignUp } = require("./SignUp");
 
 const router = require("express").Router();
-
-router.post("/signin", SignIn);
-router.post("/signup", SignUp);
+function checkUserState(req, res, next) {
+  if (req && req.cookies && req.cookies["jwt"]) {
+    let returner = {
+      result: "User entity already logged in",
+      success: false,
+    };
+    res.status(403).send(returner);
+  } else {
+    next();
+  }
+}
+router.post("/signin", [checkUserState], SignIn);
+router.post("/signup", [checkUserState], SignUp);
 router.post("/signout", (req, res) => {
-  res.status(200).send({ state: "OK", url: req.originalUrl });
+  if (req.cookies["jwt"]) {
+    res.clearCookie("jwt");
+    res.removeHeader("Authorization");
+    res.status(200).json({
+      success: true,
+      msg: "You have successfully logged out!",
+      user: req.user,
+    });
+  } else {
+    res.status(200).json({
+      success: false,
+      msg: "No User to logout",
+      user: null,
+    });
+  }
 });
 
 module.exports = router;
