71acd81d414824b0e9a36cb5edfb8f88304bd578
AdwaitNPradhan <adwaitpradhan2001@gmail.com>
2021-10-05 15:29:55 +0530

    Sign In implmented; Moving on with Sign Out with User State check

--- routes/Auth/SignIn.js
+++ routes/Auth/SignIn.js
@@ -1,6 +1,54 @@
+const { error4xx, error5xx } = require("../../Controllers/ErrResponse");
 const User = require("../../Models/User");
 const issueJWT = require("../../Utils/JWTmaker");
-const bcrypt = requrire("bcrypt");
+const bcrypt = require("bcrypt");
 exports.SignIn = (req, res, next) => {
-    
+  const { email, password } = req.body;
+  if (!email || !password) {
+    let error = error4xx("Sign In", "Email, Password fields are required");
+    console.error(error);
+    res.send(400).send(error);
+  } else {
+    User.findOne({ "contact.email": email })
+      .then((user) => {
+        if (user) {
+          if (bcrypt.compareSync(password, user.password)) {
+            const tokenObject = issueJWT("user", user);
+            res
+              .cookie("jwt", tokenObject.token)
+              .status(200)
+              .json({
+                success: true,
+                User: {
+                  uName: user.uName,
+                  fName: user.fName,
+                  lName: user.lName,
+                  contact: {
+                    email: user.contact.email,
+                    phone: user.contact.phone,
+                  },
+                },
+                token: tokenObject.token,
+              });
+          } else {
+            let error = error4xx(
+              "Login1",
+              "Either the Email or the Password you entered is wrong"
+            );
+            res.status(401).send(error);
+          }
+        } else {
+          let error = error4xx(
+            "Login2",
+            "Either the Email or the Password you entered is wrong"
+          );
+          res.status(404).send(error);
+        }
+      })
+      .catch((err) => {
+        let error = error5xx(err, "Login lookup", "Error while Login look up");
+        console.error(err);
+        res.status(500).send(error);
+      });
+  }
 };
--- routes/Auth/index.js
+++ routes/Auth/index.js
@@ -1,10 +1,9 @@
+const { SignIn } = require("./SignIn");
 const { SignUp } = require("./SignUp");
 
 const router = require("express").Router();
 
-router.post("/signin", (req, res) => {
-  res.status(200).send({ state: "OK", url: req.originalUrl });
-});
+router.post("/signin", SignIn);
 router.post("/signup", SignUp);
 router.post("/signout", (req, res) => {
   res.status(200).send({ state: "OK", url: req.originalUrl });
