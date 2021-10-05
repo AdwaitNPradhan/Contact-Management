8bc40ecaaaf4d80af27a27fec09359d27364486a
AdwaitNPradhan <adwaitpradhan2001@gmail.com>
2021-10-05 20:06:18 +0530

    updated Insomnia importable

--- routes/Auth/index.js
+++ routes/Auth/index.js
@@ -3,34 +3,34 @@ const { SignUp } = require("./SignUp");
 
 const router = require("express").Router();
 function checkUserState(req, res, next) {
-  if (req && req.cookies && req.cookies["jwt"]) {
-    let returner = {
-      result: "User entity already logged in",
-      success: false,
-    };
-    res.status(403).send(returner);
-  } else {
-    next();
-  }
+    if (req && req.cookies && req.cookies["jwt"]) {
+        let returner = {
+            result: "User entity already logged in",
+            success: false,
+        };
+        res.status(403).send(returner);
+    } else {
+        next();
+    }
 }
 router.post("/signin", [checkUserState], SignIn);
 router.post("/signup", [checkUserState], SignUp);
 router.post("/signout", (req, res) => {
-  if (req.cookies["jwt"]) {
-    res.clearCookie("jwt");
-    res.removeHeader("Authorization");
-    res.status(200).json({
-      success: true,
-      msg: "You have successfully logged out!",
-      user: req.user,
-    });
-  } else {
-    res.status(200).json({
-      success: false,
-      msg: "No User to logout",
-      user: null,
-    });
-  }
+    if (req.cookies["jwt"]) {
+        res.clearCookie("jwt");
+        res.removeHeader("Authorization");
+        res.status(200).json({
+            success: true,
+            msg: "You have successfully logged out!",
+            user: req.user,
+        });
+    } else {
+        res.status(200).json({
+            success: false,
+            msg: "No User to logout",
+            user: null,
+        });
+    }
 });
 
 module.exports = router;
--- routes/V1/index.js
+++ routes/V1/index.js
@@ -1,54 +1,54 @@
 const {
-  DeleteContact,
-  CreateContact,
-  GetContacts,
-  GetContact,
-  UpdateContact,
+    DeleteContact,
+    CreateContact,
+    GetContacts,
+    GetContact,
+    UpdateContact,
 } = require("./Contacts");
 const passport = require("passport");
 const { getProfile } = require("./profile");
 const router = require("express").Router();
 
 router.get(
-  "/contacts",
-  passport.authenticate(["User"], {
-    session: false,
-  }),
-  GetContacts
+    "/contacts",
+    passport.authenticate(["User"], {
+        session: false,
+    }),
+    GetContacts
 );
 router.get(
-  "/contacts/:cid",
-  passport.authenticate(["User"], {
-    session: false,
-  }),
-  GetContact
+    "/contacts/:cid",
+    passport.authenticate(["User"], {
+        session: false,
+    }),
+    GetContact
 );
 router.post(
-  "/contacts",
-  passport.authenticate(["User"], {
-    session: false,
-  }),
-  CreateContact
+    "/contacts",
+    passport.authenticate(["User"], {
+        session: false,
+    }),
+    CreateContact
 );
 router.put(
-  "/contacts/:cid",
-  passport.authenticate(["User"], {
-    session: false,
-  }),
-  UpdateContact
+    "/contacts/:cid",
+    passport.authenticate(["User"], {
+        session: false,
+    }),
+    UpdateContact
 );
 router.delete(
-  "/contacts/:cid",
-  passport.authenticate(["User"], {
-    session: false,
-  }),
-  DeleteContact
+    "/contacts/:cid",
+    passport.authenticate(["User"], {
+        session: false,
+    }),
+    DeleteContact
 );
 router.get(
-  "/profile",
-  passport.authenticate(["User"], {
-    session: false,
-  }),
-  getProfile
+    "/profile",
+    passport.authenticate(["User"], {
+        session: false,
+    }),
+    getProfile
 );
 module.exports = router;
